bad6d4f04b8e36256e7b90bf5561442d8d78365f
Pinku <pinkuparmer96@gmail.com>
2024-12-14 15:41:16 +0530

    first commit

---
+++ Backend/Config/connectDatabase.js
@@ -0,0 +1,15 @@
+// import mongoose from "mongoose";
+
+// export function connectionDatabase(){
+//     try {
+//         mongoose.connect(process.env.MONGO_URI)
+//         .then (res => 
+//             console.log("databaseconnect")
+//         )
+//         .catch(error =>
+//             console.log(error)
+//         )
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './Routes/formRoutes.js';
+import dotenv from 'dotenv';
+// import connectDB from './Config/connectDatabase.js';
+import mongoose from 'mongoose';
+// import { connectionDatabase } from './Config/connectDatabase.js';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors()); 
+app.use(express.json()); 
+
+mongoose.connect(process.env.MONGO_URI)
+        .then (res => 
+            console.log("database connect")
+        )
+        .catch(error =>
+            console.log(error)
+        )
+
+// Routes
+app.use('/api', routes);
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    // connectionDatabase();
+});
